857332c9e252962a0d317087a418f790fee415a3
DewanaGustavus <76590469+DewanaGustavus@users.noreply.github.com>
2023-11-16 20:45:17 +0700

    fix: SOAP Caller

--- src/handler/soap-caller/SoapCaller.ts
+++ src/handler/soap-caller/SoapCaller.ts
@@ -1,8 +1,9 @@
 import axios from "axios";
 import { parseXML } from "../../utils/XMLConverter";
 
-export const PESANAN_URL = "http://localhost:6001/ws/pesanan?wsdl";
-export const DETAIL_PESANAN_URL = "http://host.docker.internal:6001/ws/detailPesanan?wsdl";
+const SOAP_URL = "http://host.docker.internal:6001/ws";
+export const PESANAN_URL = SOAP_URL +  "/pesanan";
+export const DETAIL_PESANAN_URL = SOAP_URL +  "/detailPesanan";
 const REST_API_KEY = "mZHA8N63GOH88EnQW3hJ6zcJQhzY79WnuM+UnVG/e1k=";
 
 type Header = {
@@ -13,16 +14,11 @@ export async function SoapApiCall(url : string, method : string, params? : Objec
     const headers: Header = {
         'Content-Type': 'text/xml;charset=UTF-8',
         SOAPAction: '#POST',
-        'api-key': REST_API_KEY
+        'apiKey': REST_API_KEY
     };
 
     const XMLRequest = buildXMLRequest(method, params);
-    console.log("url", url);
-    console.log("XMLRequest", XMLRequest);
-    console.log("headers", headers);
-
     const response = await axios.post(url, XMLRequest, {headers});
-    console.log("response", response);
     
     return parseXML(response.data, method);
 }
